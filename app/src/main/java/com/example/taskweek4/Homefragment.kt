97a6c3f6dd622ca6f43b9afee3d0c32edab5b3d3
Yousef <yousefshafik234@yahoo.com>
2020-09-19 21:43:30 +0200

    added navigation and home fragment recycler view

--- app/src/main/java/com/example/taskweek4/Homefragment.kt
+++ app/src/main/java/com/example/taskweek4/Homefragment.kt
@@ -5,55 +5,105 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.navigation.navGraphViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.taskweek4.data.models.ui.Movies
+import com.example.taskweek4.recyclerview.MovieAdabter
+import com.example.taskweek4.ui.MovieViewModel
+import com.example.taskweek4.ui.homeActivity
+import kotlinx.android.synthetic.main.fragment_homefragment.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Homefragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Homefragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private val movieViewModel: MovieViewModel by viewModels()
+    private val movieAdabter: MovieAdabter = MovieAdabter(mutableListOf())
+    private var page=1
+    private var isFirst =true
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+
         return inflater.inflate(R.layout.fragment_homefragment, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Homefragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Homefragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        displayData()
+        setRecyclerView()
+        spinnerListener()
+        movieViewModel.loadMovieData("movie",page)
+        scrollListener()
+
+        movieViewModel.movieLiveData.observe(
+            viewLifecycleOwner,
+            {
+                bindData(it)
+
+            }
+        )
+    }
+
+    private fun setRecyclerView() {
+        movieRecyclerView.apply {
+            adapter = movieAdabter
+            layoutManager = LinearLayoutManager(this.context, RecyclerView.VERTICAL, false)
+        }
+    }
+
+    private fun bindData(movies: List<Movies>) {
+        movieAdabter.addData(movies)
+    }
+
+    private fun displayData() {
+        val list = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_list_item_1, resources.getStringArray(R.array.spinner2)
+        )
+
+        list.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinner1.adapter = list
+    }
+    private fun spinnerListener(){
+        spinner1.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                if(isFirst) {
+                    isFirst = false
+                    return
+                }
+                page=1
+                movieAdabter.clearData()
+                setRecyclerView()
+                movieViewModel.loadMovieData(spinner1.selectedItem.toString(),page)
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                movieViewModel.loadMovieData("movie",page)
+            }
+
+        }
+
+    }
+
+    private fun scrollListener(){
+        movieRecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                if(!movieRecyclerView.canScrollVertically(1)&&!movieViewModel.isLoading()){
+                    page++
+
+                    movieViewModel.loadMovieData(spinner1.selectedItem.toString(),page = page)
                 }
             }
+
+        })
     }
+
+
 }
--- app/src/main/java/com/example/taskweek4/ui/homeActivity.kt
+++ app/src/main/java/com/example/taskweek4/ui/homeActivity.kt
@@ -1,108 +1,28 @@
 package com.example.taskweek4.ui
 
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import com.example.taskweek4.R
-import com.example.taskweek4.recyclerview.MovieAdabter
 import kotlinx.android.synthetic.main.mainactivity.*
-
-
 class homeActivity : AppCompatActivity() {
-    private val movieViewModel: MovieViewModel by viewModels()
-    private val movieAdabter:MovieAdabter = MovieAdabter(mutableListOf())
-    private var page=1
-    private var isFirst =true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.mainactivity)
 
-        displayData()
+        setContentView(R.layout.mainactivity)
 
         val navController = Navigation
             .findNavController(this, R.id.mainFragment)
 
         NavigationUI.setupWithNavController(navigationBar, navController)
-//        setRecyclerView()
-//        movieViewModel.loadMovieData("movie",page)
-//
-//
-//        movieViewModel.movieLiveData.observe(
-//            this,
-//            {
-//                bindData(it)
-//
-//            }
-//        )
-//
-//        spinnerListener()
-//        scrollListener()
-//
+
+
 
 
     }
-
-//    private fun bindData(movies: List<Movies>) {
-//        movieAdabter.addData(movies)
-//    }
-//
-//    private fun scrollListener(){
-//        movieRecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-//            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-//                if(!movieRecyclerView.canScrollVertically(1)&&!movieViewModel.isLoading()){
-//                    page++
-//                    movieViewModel.loadMovieData(spinner1.selectedItem.toString(),page = page)
-//                }
-//            }
-//
-//        })
-//    }
-//
-//
-//    private fun setRecyclerView() {
-//        movieRecyclerView.apply {
-//            adapter = movieAdabter
-//            layoutManager = LinearLayoutManager(this.context, RecyclerView.VERTICAL, false)
-//        }
-//    }
-
-    private fun displayData() {
-        val list = ArrayAdapter(
-            this,
-            android.R.layout.simple_list_item_1, resources.getStringArray(R.array.spinner2)
-        )
-
-        list.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
-        spinner1.adapter = list
-    }
-
-//    private fun spinnerListener(){
-//        spinner1.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-//            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-//                if(isFirst) {
-//                    isFirst = false
-//                    return
-//                }
-//                page=1
-//                movieAdabter.clearData()
-//                setRecyclerView()
-//                movieViewModel.loadMovieData(spinner1.selectedItem.toString(),page)
-//            }
-//
-//            override fun onNothingSelected(p0: AdapterView<*>?) {
-//
-//            }
-//
-//        }
-
-   // }
-
-
 }
 
 

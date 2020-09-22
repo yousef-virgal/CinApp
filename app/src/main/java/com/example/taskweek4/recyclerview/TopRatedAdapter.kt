b7664a319477e5f763d3e877b6b47bb16d3f58e9
Yousef <yousefshafik234@yahoo.com>
2020-09-22 21:16:34 +0200

    added carousel view in top rated and changed th adapter to handle the new view

--- app/src/main/java/com/example/taskweek4/recyclerview/TopRatedAdapter.kt
+++ app/src/main/java/com/example/taskweek4/recyclerview/TopRatedAdapter.kt
@@ -4,21 +4,34 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.taskweek4.R
 import com.example.taskweek4.data.models.ui.Movies
 import com.example.taskweek4.ui.Activity.ItemActivity
 import com.squareup.picasso.Picasso
+import com.synnapps.carouselview.CarouselView
+import com.synnapps.carouselview.ImageClickListener
+import com.synnapps.carouselview.ImageListener
 
 class TopRatedAdapter(val movies:MutableList<Movies>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     private lateinit var intent: Intent
 
-    class TopRatedVieHolder(itemView: View): RecyclerView.ViewHolder(itemView)
-
     private val titleViewHolder:Int =0
     private val normalViewHolder:Int =1
     private val loadingViewHolder:Int =2
+    private val carouselViewHolder:Int = 3
+    private val blankViewHolder:Int = 4
+    private lateinit var list:List<Int>
+
+    class TopRatedVieHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+    class CarouselViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val carousel:CarouselView = itemView.findViewById(R.id.carouselView)
+    }
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
         return when(viewType){
             titleViewHolder->{
                 val v = LayoutInflater.from(parent.context).inflate(R.layout.topratedtitle,parent,false)
@@ -32,6 +45,14 @@ class TopRatedAdapter(val movies:MutableList<Movies>): RecyclerView.Adapter<Recy
                 val v = LayoutInflater.from(parent.context).inflate(R.layout.loading,parent,false)
                 MovieAdabter.LoadingViewHolder(v)
             }
+            carouselViewHolder->{
+                val v = LayoutInflater.from(parent.context).inflate(R.layout.carouselview,parent,false)
+                return CarouselViewHolder(v)
+            }
+            blankViewHolder->{
+                val v = LayoutInflater.from(parent.context).inflate(R.layout.blank,parent,false)
+                return MovieAdabter.BlankViewHolder(v)
+            }
             else->{
                 val v = LayoutInflater.from(parent.context).inflate(R.layout.fragment_toprated,parent,false)
                 MovieAdabter.NormalMovieHolder(v)
@@ -40,7 +61,27 @@ class TopRatedAdapter(val movies:MutableList<Movies>): RecyclerView.Adapter<Recy
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        if(holder is MovieAdabter.NormalMovieHolder){
+        if(holder is CarouselViewHolder){
+            holder.carousel.pageCount =3
+            holder.carousel.setImageListener { myPosition, imageView ->
+                Picasso.get()
+                    .load("http://image.tmdb.org/t/p/w500" + movies[myPosition].posterPath)
+                    .into(imageView)
+            }
+
+            holder.carousel.setImageClickListener { position ->
+                intent = Intent(
+                    holder.itemView.context,
+                    ItemActivity::class.java
+                )
+                intent.putExtra("title", movies[position].title)
+                intent.putExtra("overViewText", movies[position].overview)
+                intent.putExtra("Image", movies[position].backdropPath)
+                holder.itemView.context.startActivity(intent)
+            }
+        }
+
+        else if(holder is MovieAdabter.NormalMovieHolder){
             holder.itemView.setOnClickListener {
                 intent = Intent(
                     holder.itemView.context,
@@ -70,6 +111,8 @@ class TopRatedAdapter(val movies:MutableList<Movies>): RecyclerView.Adapter<Recy
         return when(position){
             0->titleViewHolder
             movies.size-1->loadingViewHolder
+            1->carouselViewHolder
+            2,3->blankViewHolder
             else->normalViewHolder
         }
     }
